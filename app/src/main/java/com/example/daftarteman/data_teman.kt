3778638c566560362437f655319508973a238475
Oky Mikhael <okymikhael@gmail.com>
2021-06-02 21:31:45 +0700

    Create Function

---
+++ app/src/main/java/com/example/daftarteman/data_teman.kt
@@ -0,0 +1,17 @@
+package com.example.daftarteman
+
+class data_teman {
+    var nama: String? = null
+    var alamat: String? = null
+    var noHp: String? = null
+    var key: String? = null
+
+    constructor()
+
+    constructor(nama: String?, alamat: String?, noHp: String?) {
+        this.nama = nama
+        this.alamat = alamat
+        this.noHp = noHp
+
+    }
+}
